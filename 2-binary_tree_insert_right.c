3f819aa8073eb610026e4af61b2ef8bdc4ba2acf
MustaphaHm <mustapha.houmam@gmail.com>
2023-08-30 10:12:30 -0700

    Coorecting betty issues in task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent == NUL)
+	if (parent == NULL)
 		return (NULL);
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
