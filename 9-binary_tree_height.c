b916e89bd784ebd64c44060a0126469b0941252a
MustaphaHm <mustapha.houmam@gmail.com>
2023-08-30 11:38:21 -0700

    Task 9, function that calculate the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,9 +8,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lf_h = 0, r_h = 0;
 
-	if (tree == NULL)
-		return (-1);
-	lf_h = binary_tree_height(tree->left);
-	r_h = binary_tree_height(tree->right);
-	return (1 + (lf_h > r_h ? lf_h : r_h));
+	if (tree)
+	{
+		lf_h = 1 + binary_tree_height(tree->left);
+		r_h = 1 + binary_tree_height(tree->right);
+		return ((lf_h > r_h ? lf_h : r_h));
+	}
+	return (0);
 }
